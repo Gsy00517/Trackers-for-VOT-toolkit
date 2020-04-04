5385d2f18ad5cea96b5d9d727ede067d7820b256
Gsy00517 <917547962@qq.com>
2020-04-04 09:49:48 +0800

    delete some redundant files

--- matlab_trackers/ECO/tracker_ECO.m
+++ matlab_trackers/ECO/tracker_ECO.m
@@ -1,7 +1,7 @@
 % Copy this template configuration file to your VOT workspace.
 % Enter the full path to the ECO repository root folder.
 
-tracker_label = 'ECO';
+tracker_label = ['ECO'];
 
 tracker_command = generate_matlab_command('eco(''ECO'', ''VOT2016_DEEP_settings'', true)', {'abs_path/ECO'});
 
